43e05b2bbd8a726a7cdf2105f7cadf80b269d531
Squidfishrl <48411473+Squidfishrl@users.noreply.github.com>
2020-07-20 04:24:27 +0300

    added math functions frame

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,5 @@
 import tkinter
+from tkinter import ttk
 import keyboard
 import math
 import datetime
@@ -27,19 +28,26 @@ class CalculatorMain:
         self.magicFrame.config(height=0, width=screenWidth)
         self.magicFrame.grid(row=0, column=0, sticky="WE")
 
-        self.numberLayoutFrame = tkinter.Frame(main)
-        self.numberLayoutFrame.config(height=20)
-        self.numberLayoutFrame.grid(row=1, column=0, sticky="WE")
-        self.numberLayoutFrame.grid_remove()
+        # self.mathFunctionsFrame = tkinter.Frame(main)
+        # self.mathFunctionsFrame.config(height=80, bg="red")
+        # self.mathFunctionsFrame.grid(row=1, column=0)
+        # self.mathFunctionsFrame.grid_remove()
+
+        self.numberAndFuncLayoutFrame = tkinter.Frame(main)
+        self.numberAndFuncLayoutFrame.config(height=20)
+        self.numberAndFuncLayoutFrame.grid(row=2, column=0, sticky="WE")
+        self.numberAndFuncLayoutFrame.grid_remove()
 
         self.buttonMenuFrame = tkinter.Frame(main)
         self.buttonMenuFrame.config(height=20)
-        self.buttonMenuFrame.grid(row=2, column=0, sticky="WE")
+        self.buttonMenuFrame.grid(row=3, column=0, sticky="WE")
 
         self.inputFrame = tkinter.Frame(main)
         self.inputFrame.config(height=40)
-        self.inputFrame.grid(row=3, column=0, sticky="WE")
+        self.inputFrame.grid(row=4, column=0, sticky="WE")
         self.inputFrame.focus_set()
+
+
 
 
     def showANDhide(self, main, event=None):
@@ -55,8 +63,6 @@ class CalculatorInput:
     def __init__(self, frame):
         self.userInputEntry = tkinter.Entry(frame, font=("Helvetica", 20), takefocus=1)
         self.userInputEntry.pack(fill=tkinter.BOTH)
-        self.userInputEntry.bind("<Up>", lambda event, key="up": self.traverseHistoryUpDown(event, key))
-        self.userInputEntry.bind("<Down>", lambda event, key="down": self.traverseHistoryUpDown(event, key))
         self.upAndDownKeyPress = 0
 
     def calculateUserInput(self, event=None):
@@ -130,34 +136,35 @@ class CalculatorButtonMenu:
 
     def __init__(self, frame, main):
 
-
-
         self.btnEquals = tkinter.Button(frame, text="=", command=lambda: calcInput.calculateUserInput())
         self.btnEquals.pack(side="left")
 
-        self.numberLayoutPopUp = tkinter.Button(frame, text="🠕", command=lambda: self.popUpCommonCharWindow(main))
+        self.numberLayoutPopUp = tkinter.Button(frame, text="🠕", command=lambda: self.popUpMoreFuncFrame(main))
         self.numberLayoutPopUp.pack(side="left")
 
         self.btnClear = tkinter.Button(frame, text="C", command=lambda: calcInput.clearUserInput())
         self.btnClear.pack(side="left")
 
-        self.btnDot = tkinter.Button(frame, text=".", command=lambda: calcNumberLayout.insertInUserInputEntry(self.btnDot.cget('text')))
+        self.btnDot = tkinter.Button(frame, text=".", command=lambda: calcNumberAndFuncLayout.insertInUserInputEntry(self.btnDot.cget('text')))
 
-        self.btnPlus = tkinter.Button(frame, text="+", command=lambda: calcNumberLayout.insertInUserInputEntry(self.btnPlus.cget('text')))
+        self.btnPlus = tkinter.Button(frame, text="+", command=lambda: calcNumberAndFuncLayout.insertInUserInputEntry(self.btnPlus.cget('text')))
 
-        self.btnMinus = tkinter.Button(frame, text="-", command=lambda: calcNumberLayout.insertInUserInputEntry(self.btnMinus.cget('text')))
+        self.btnMinus = tkinter.Button(frame, text="-", command=lambda: calcNumberAndFuncLayout.insertInUserInputEntry(self.btnMinus.cget('text')))
 
-        self.btnMultiply = tkinter.Button(frame, text="*", command=lambda: calcNumberLayout.insertInUserInputEntry(self.btnMultiply.cget('text')))
+        self.btnMultiply = tkinter.Button(frame, text="*", command=lambda: calcNumberAndFuncLayout.insertInUserInputEntry(self.btnMultiply.cget('text')))
 
-        self.btnDivide = tkinter.Button(frame, text="/", command=lambda: calcNumberLayout.insertInUserInputEntry(self.btnDivide.cget('text')))
+        self.btnDivide = tkinter.Button(frame, text="/", command=lambda: calcNumberAndFuncLayout.insertInUserInputEntry(self.btnDivide.cget('text')))
 
-        self.btnGrade = tkinter.Button(frame, text="^", command=lambda: calcNumberLayout.insertInUserInputEntry("**"))
+        self.btnComma = tkinter.Button(frame, text=",", command=lambda: calcNumberAndFuncLayout.insertInUserInputEntry(self.btnComma.cget('text')))
 
         self.exitButton = tkinter.Button(frame, text="x", command=lambda: self.closeMainWindow(main))
         self.exitButton.pack(side="right")
 
         self.histViewButton = tkinter.Button(frame, text="History", command=lambda: self.viewCalcHistory(main))
         self.histViewButton.pack(side="right")
+
+        self.mathLayoutPopUp = tkinter.Button(frame, text="🠕🠕", command=lambda: calcNumberAndFuncLayout.mathFunctionsPopUp(main))
+        self.mathLayoutPopUp.pack(side="right")
 
     def closeMainWindow(self, main):
         main.destroy()
@@ -175,33 +182,49 @@ class CalculatorButtonMenu:
             calcHistoryMenu.createWindow(main)
             self.histViewButton["text"] = "Close History"
 
-    def popUpCommonCharWindow(self, main):
-        if calc.numberLayoutFrame.winfo_ismapped():
-            calc.numberLayoutFrame.grid_remove()
-            master.geometry("%sx67+0+%s" % (screenWidth, screenHeight-40))
+    def popUpMoreFuncFrame(self, main):
+        if calc.numberAndFuncLayoutFrame.winfo_ismapped():
+            calc.numberAndFuncLayoutFrame.grid_remove()
+            main.geometry("%sx67+0+%s" % (screenWidth, screenHeight-40))
 
             self.btnDot.pack_forget()
             self.btnPlus.pack_forget()
             self.btnMinus.pack_forget()
             self.btnMultiply.pack_forget()
             self.btnDivide.pack_forget()
-            self.btnGrade.pack_forget()
+            self.btnComma.pack_forget()
 
         else:
             main.geometry("%sx98+0+%s" % (screenWidth, screenHeight-40))
-            calc.numberLayoutFrame.grid()
+            calc.numberAndFuncLayoutFrame.grid()
 
             self.btnDot.pack(side="left")
             self.btnPlus.pack(side="left")
             self.btnMinus.pack(side="left")
             self.btnMultiply.pack(side="left")
             self.btnDivide.pack(side="left")
-            self.btnGrade.pack(side="left")
+            self.btnComma.pack(side="left")
             self.btnClear.pack_forget()
             self.btnClear.pack(side="left")
 
+    # def popUpMathFunctionFrame(self, main):
+    #     if calc.mathFunctionsFrame.winfo_ismapped():
+    #         calc.mathFunctionsFrame.grid_remove()
+    #         if calc.numberAndFuncLayoutFrame.winfo_ismapped():
+    #             main.geometry("%sx98+0+%s" % (screenWidth, screenHeight-40))
+    #         else:
+    #             main.geometry("%sx67+0+%s" % (screenWidth, screenHeight-40))
+    #
+    #     else:
+    #         if calc.numberAndFuncLayoutFrame.winfo_ismapped():
+    #             main.geometry("%sx179+0+%s" % (screenWidth, screenHeight-40))
+    #         else:
+    #             main.geometry("%sx148+0+%s" % (screenWidth, screenHeight-40))
+    #
+    #         calc.mathFunctionsFrame.grid()
 
-class CalculatorNumberLayout:
+
+class CalculatorNumberAndFunctionLayout:
 
     def __init__(self, frame):
 
@@ -234,8 +257,153 @@ class CalculatorNumberLayout:
         self.btnClearOne = tkinter.Button(frame, text="<-", command=lambda: calcInput.clearLastInputedChar())
         self.btnClearOne.pack(side="left")
 
+        self.optFrame = tkinter.Frame(frame)
+
+        self.optPossibilitiesValues = ["combination(n,k)", "permutation(n,k)", "factorial(x)"]
+        self.optPossibilities = ttk.Combobox(self.optFrame, values=self.optPossibilitiesValues)
+        self.optPossibilities.state = (['disabled'])
+        self.optPossibilities.set('possibilities')
+        self.optPossibilities.bind("<Key>", lambda e: "break")
+        self.optPossibilities.bind("<<ComboboxSelected>>", lambda event: self.detectMathFunction(event, self.optPossibilities.get()))
+        self.optPossibilities.pack(side="right")
+
+        self.optTrigonometryValues = ["sin(x)", "cos(x)", "tan(x)", "asin(x)", "acos(x)", "atan(x)"]
+        self.optTrigonometry = ttk.Combobox(self.optFrame, values=self.optTrigonometryValues)
+        self.optTrigonometry.state = (['disabled'])
+        self.optTrigonometry.set('trigonometry')
+        self.optTrigonometry.bind("<Key>", lambda e: "break")
+        self.optTrigonometry.bind("<<ComboboxSelected>>", lambda event: self.detectMathFunction(event, self.optTrigonometry.get()))
+        self.optTrigonometry.pack(side="right")
+
+        self.optAngularConversionValues = ["radians", "degrees"]
+        self.optAngularConversion = ttk.Combobox(self.optFrame, values=self.optAngularConversionValues)
+        self.optAngularConversion.state = (['disabled'])
+        self.optAngularConversion.set('angular conversion')
+        self.optAngularConversion.bind("<Key>", lambda e: "break")
+        self.optAngularConversion.bind("<<ComboboxSelected>>", lambda event: self.detectMathFunction(event, self.optAngularConversion.get()))
+        self.optAngularConversion.pack(side="right")
+
+        self.optHyperbolicValues = ["sinh(x)", "cosh(x)", "tanh(x)", "asinh(x)", "acosh(x)", "atanh(x)"]
+        self.optHyperbolic = ttk.Combobox(self.optFrame, values=self.optHyperbolicValues)
+        self.optHyperbolic.state = (['disabled'])
+        self.optHyperbolic.set('hyperbolic')
+        self.optHyperbolic.bind("<Key>", lambda e: "break")
+        self.optHyperbolic.bind("<<ComboboxSelected>>", lambda event: self.detectMathFunction(event, self.optHyperbolic.get()))
+        self.optHyperbolic.pack(side="right")
+
+        self.optOtherValues = ["pow(x, y)", "sqrt(x)", "log(x, base)"]
+        self.optOther = ttk.Combobox(self.optFrame, values=self.optOtherValues)
+        self.optOther.state = (['disabled'])
+        self.optOther.set('other')
+        self.optOther.bind("<Key>", lambda e: "break")
+        self.optOther.bind("<<ComboboxSelected>>", lambda event: self.detectMathFunction(event, self.optOther.get()))
+        self.optOther.pack(side="right")
+
     def insertInUserInputEntry(self, char):
         calcInput.userInputEntry.insert(tkinter.END, char)
+
+    def detectMathFunction(self, event, selection):
+        if selection in self.optPossibilitiesValues:
+            self.optPossibilities.set('possibilities')
+            if "comb" in selection:
+                # math.comb(n,k)
+                self.insertMathFunctionInEntry("math.comb()")
+            elif "perm" in selection:
+                # math.perm(n,k) k is optional - without k returns n factorial
+                self.insertMathFunctionInEntry("math.perm()")
+            elif "factorial" in selection:
+                # math.factorial(x)
+                self.insertMathFunctionInEntry("math.factorial()")
+
+        elif selection in self.optTrigonometryValues:
+            self.optTrigonometry.set('trigonometry')
+            if selection == "sin(x)":
+                # math.sin(x)
+                self.insertMathFunctionInEntry("math.radians()")
+                self.insertMathFunctionInEntry("math.sin()")
+            elif selection == "cos(x)":
+                # math.cos(x)
+                self.insertMathFunctionInEntry("math.radians()")
+                self.insertMathFunctionInEntry("math.cos()")
+            elif selection == "tan(x)":
+                # math.tan(x)
+                self.insertMathFunctionInEntry("math.radians()")
+                self.insertMathFunctionInEntry("math.tan()")
+            elif selection == "asin(x)":
+                # math.asin(x)
+                self.insertMathFunctionInEntry("math.radians()")
+                self.insertMathFunctionInEntry("math.asin()")
+            elif selection == "acos(x)":
+                # math.acos(x)
+                self.insertMathFunctionInEntry("math.radians()")
+                self.insertMathFunctionInEntry("math.acos()")
+            elif selection == "atan(x)":
+                # math.atan(x)
+                self.insertMathFunctionInEntry("math.radians()")
+                self.insertMathFunctionInEntry("math.atan()")
+
+        elif selection in self.optAngularConversionValues:
+            self.optAngularConversion.set("angular conversion")
+            if "degrees" in selection:
+                # math.degrees() - converts radians to degrees
+                self.insertMathFunctionInEntry("math.degrees()")
+
+            elif "radians" in selection:
+                # opposite of math.degrees
+                self.insertMathFunctionInEntry("math.radians()")
+
+        elif selection in self.optHyperbolicValues:
+            self.optHyperbolic.set('hyperbolic')
+            if selection == "sinh(x)":
+                # math.sinh(x)
+                self.insertMathFunctionInEntry("math.sinh()")
+            elif selection == "cosh(x)":
+                # math.cosh(x)
+                self.insertMathFunctionInEntry("math.cosh()")
+            elif selection == "tanh(x)":
+                # math.tanh(x)
+                self.insertMathFunctionInEntry("math.tanh()")
+            elif selection == "asinh(x)":
+                # math.asinh(x)
+                self.insertMathFunctionInEntry("math.asinh()")
+            elif selection == "acosh(x)":
+                # math.acosh(x)
+                self.insertMathFunctionInEntry("math.acosh()")
+            elif selection == "atanh(x)":
+                # math.atanh(x)
+                self.insertMathFunctionInEntry("math.atanh()")
+
+        elif selection in self.optOtherValues:
+            self.optOther.set("other")
+            if "pow" in selection:
+                # math.pow(x,y)
+                self.insertMathFunctionInEntry("math.pow()")
+            elif "sqrt" in selection:
+                # math.sqrt(x)
+                self.insertMathFunctionInEntry("math.sqrt()")
+            elif "log" in selection:
+                # math.log(x, base)/math.log(x)
+                self.insertMathFunctionInEntry("math.log()")
+
+    def insertMathFunctionInEntry(self, command):
+        entryMsg = calcInput.userInputEntry.get()
+        entryMsg = command[:-1] + entryMsg + ")"
+        calcInput.userInputEntry.delete(0, tkinter.END)
+        calcInput.userInputEntry.insert(0, entryMsg)
+
+    def mathFunctionsPopUp(self, main):
+        if calc.numberAndFuncLayoutFrame.winfo_ismapped():
+            pass
+        else:
+            calc.numberAndFuncLayoutFrame.grid()
+            main.geometry("%sx98+0+%s" % (screenWidth, screenHeight-40))
+
+        if self.optFrame.winfo_ismapped():
+            self.optFrame.pack_forget()
+        else:
+            self.optFrame.pack(side="right")
+
+
 
 
 class CalculatorHistoryMenu:
@@ -272,16 +440,17 @@ class CalculatorHistoryMenu:
         calcButtonMenu.histViewButton["text"] = "History"
 
 
-global calc, calcButtonMenu, calcInput, calcNumberLayout, calcHistoryMenu
+global calc, calcButtonMenu, calcInput, calcNumberAndFuncLayout, calcHistoryMenu
 calc = CalculatorMain(master)
 calcInput = CalculatorInput(calc.inputFrame)
 calcHistoryMenu = CalculatorHistoryMenu()
 calcButtonMenu = CalculatorButtonMenu(calc.buttonMenuFrame, master)
-calcNumberLayout = CalculatorNumberLayout(calc.numberLayoutFrame)
-
+calcNumberAndFuncLayout = CalculatorNumberAndFunctionLayout(calc.numberAndFuncLayoutFrame)
 
 
 keyboard.add_hotkey("ctrl+space", lambda: calc.showANDhide(master))
 master.bind('<Return>', lambda event: calcInput.calculateUserInput())
+calcInput.userInputEntry.bind("<Up>", lambda event, key="up": calcInput.traverseHistoryUpDown(event, key))
+calcInput.userInputEntry.bind("<Down>", lambda event, key="down": calcInput.traverseHistoryUpDown(event, key))
 
 master.mainloop()
